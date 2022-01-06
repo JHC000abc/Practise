fd572b2b13ed636a3b82dc8438d7500a66cb1937
‘jiaohaicheng’ <jhcheng@microcreative.org>
2022-01-06 14:36:54 +0800

    commit 3

--- 学习/test002.py
+++ 学习/test002.py
@@ -5,7 +5,9 @@ from collections import Counter
 import matplotlib.pyplot as plt
 
 
-
+'''
+https://www.cjavapy.com/article/1146/
+'''
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
 # 平均数
@@ -45,5 +47,19 @@ data = np.random.uniform(-1,1,10)
 print(data)
 print(pd.value_counts(data))
 plt.hist(data,10)
+# plt.show()
+
+# 正态数据分布 (平均值，标准差)
+data2 = np.random.normal(5.0,1.0,100000)
+print('data2=',data2)
+plt.hist(data2,100)
+# plt.show()
+
+# 散点 scatter
+x = np.random.randint(1,10,100)
+y = np.random.randint(1,10001,100)
+plt.scatter(x,y)
 plt.show()
+
+# 回归
 
